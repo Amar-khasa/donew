d28a613eed643fa833249049509db80fa998cbe0
Amar Khasa <63017115+Amar-khasa@users.noreply.github.com>
2020-06-09 19:25:45 +0530

    Add files via upload

---
+++ github/github.notification.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>github</title>
+</head>
+<body>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Quicksand:wght@500&display=swap" rel="stylesheet"> 
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<style type="text/css">	
+body
+{
+font-family: 'Open Sans', sans-serif;
+font-family: 'Quicksand', sans-serif;
+font-size:18px;                                                             
+overflow: hidden;
+}
+</style>
+
+<style type="text/css">
+.btn
+{
+margin-top:21px;
+border-top-right-radius:2px; 		
+border: 1px solid rgba(27,31,35,.2);
+line-height:21px;
+background-image: linear-gradient(-360deg,#fafbfc,#eff3f6 90%);
+color:#24292e;
+height:32px;
+overflow: hidden;
+padding:5px 3px;
+line-height:1.1;
+box-sizing:border-box;
+font-weight:500;
+font-size:16px;
+width:81px;
+letter-spacing:1px;
+height:36px;
+
+
+}
+.mop
+{
+margin-left:55px;
+margin-right:5px;
+margin-top:10px;
+}
+
+.btn:hover
+{
+background-color:black;
+}
+.input-group
+{
+margin-top:10px;
+margin-left:40px;
+height:32px;
+border-radius:3px; 
+font-weight:100px;
+opacity:0.4;
+width:600px;
+}
+.ui
+{
+height: 60px;
+}
+
+.right-mop
+{
+margin-top:10px;
+border-top-right-radius:2px; 	
+background-color:#dedef0;
+margin-left:18px;
+margin-right:5px;
+margin-top:20px;
+font-size:15px;
+font-family:sans-serif;
+letter-spacing:1px;
+width:140px;
+height:37px;
+text-align:center;
+font-weight:400;
+}
+.p
+{
+	font-weight:500;
+}
+
+</style>
+<div class="container-fluid ui">
+<div class="container">
+<div class="btn float-left"style="width:43px;">All</div>
+<div class="btn float-left">Unread</div>
+<div class="container filter">
+<div class="mop float-left">
+	<div class="input-group mb-3">
+  <div class="input-group-prepend">
+    <span class="input-group-text border" id="basic-addon1"style="background-color:#fafbfc;"><svg class='bi bi-filter' width='1em' height='1em' viewBox='0 0 16 16' fill='currentColor' xmlns='http://www.w3.org/2000/svg'>
+  <path fill-rule='evenodd' d='M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z'/>
+</svg></span>
+  </div>
+  <input type="text" class="form-control" placeholder="Filter" aria-label="Username" aria-describedby="basic-addon1"style="background-color:#fafbfc;">
+</div>
+
+
+</div>
+</div>
+<div class="btn right-mop float-left text-center pt-2">Group by:<strong>Date</strong></div>
+</div>
+</div>
+
+<style type="text/css">
+.front-img
+{
+padding-left:10px;
+margin-left:320px;
+margin-top:150px;
+
+}
+img
+{
+width:550px;
+}		
+.content
+{
+text-align:center;
+margin-top:25px;
+font-size:27px;
+
+
+}
+
+</style>
+<div class="container-fluid">
+<div class="container">
+<div class="front-img">
+<img src="./img/inbox-zero.png">
+</div>
+<div class="content">
+All caught up!<br><p>Take a break, write some code, do what you do best.<p>
+</div>
+</div>			
+</div>	
+
+
+</body>
+</html>
